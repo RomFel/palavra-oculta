13f543eb18b9c435adde49c8f481dc7aeed99479
RomFel <rom.fel07@gmail.com>
2025-07-24 08:56:33 -0300

    refatorando banco de dados

---
+++ data.py
@@ -0,0 +1,18 @@
+dica = ['cor', 'fruta', 'cidade', 'time', 'objeto', 'animal']
+lista_palavras = [
+    ['verde', 'amarelo', 'azul', 'vermelho','laranja', 'marrom', 'preto', 'branco', 'rosa', 'roxo'],
+    
+    ['manga', 'banana', 'morango', 'uva', 'abacate', 'kiwi', 'goiaba', 'abacaxi'],
+    
+    ['maxaranguape','olinda', 'recife', 'natal', 'londres', 'roma', 'paris', 'berlim', 'lisboa', 'touros',
+        'parnamirim', 'chicago', 'toronto', 'extremoz', 'pequim', 'sydney'],
+    
+    ['palmeiras','fluminense', 'flamengo', 'sport', 'santos', 'internacional', 'corinthians', 'botafogo',
+        'cruzeiro', 'vasco', 'liverpool', 'arsenal', 'milan', 'barcelona', 'roma', 'benfica', 'porto', 'sporting', 'borussia', 'bayern'],
+    
+    ['bolsa', 'borracha', 'camisa', 'sapato', 'bola', 'caneta', 'cadeira', 'dado', 'escova', 'livro',
+        'caderno', 'controle', 'mesa', 'geladeira', 'carro', 'computador', 'gravata', 'controle', 'microfone'],
+    
+    ['abelha', 'gato', 'pato', 'tigre', 'elefante', 'zebra', 'cachorro', 'canguru', 'galinha', 'papagaio',
+        'peixe', 'cobra', 'urso', 'lula', 'baleia', 'carangueijo', 'lagosta', 'andorinha', 'urubu', 'porco']
+]
--- palavra-oculta.py
+++ palavra-oculta.py
@@ -1,4 +1,5 @@
 import os
+from data import dica, lista_palavras
 
 def titulo():
     print('-'*50)
@@ -33,24 +34,7 @@ while True:
 
         pts_totais = 0
 
-        dica = ['cor', 'fruta', 'cidade', 'time', 'objeto', 'animal']
-        lista_palavras = [
-            ['verde', 'amarelo', 'azul', 'vermelho','laranja', 'marrom', 'preto', 'branco', 'rosa', 'roxo'],
-            
-            ['manga', 'banana', 'morango', 'uva', 'abacate', 'kiwi', 'goiaba', 'abacaxi'],
-            
-            ['maxaranguape','olinda', 'recife', 'natal', 'londres', 'roma', 'paris', 'berlim', 'lisboa', 'touros',
-             'parnamirim', 'chicago', 'toronto', 'extremoz', 'pequim', 'sydney'],
-            
-            ['palmeiras','fluminense', 'flamengo', 'sport', 'santos', 'internacional', 'corinthians', 'botafogo',
-             'cruzeiro', 'vasco', 'liverpool', 'arsenal', 'milan', 'barcelona', 'roma', 'benfica', 'porto', 'sporting', 'borussia', 'bayern'],
-            
-            ['bolsa', 'borracha', 'camisa', 'sapato', 'bola', 'caneta', 'cadeira', 'dado', 'escova', 'livro',
-             'caderno', 'controle', 'mesa', 'geladeira', 'carro', 'computador', 'gravata', 'controle', 'microfone'],
-            
-            ['abelha', 'gato', 'pato', 'tigre', 'elefante', 'zebra', 'cachorro', 'canguru', 'galinha', 'papagaio',
-             'peixe', 'cobra', 'urso', 'lula', 'baleia', 'carangueijo', 'lagosta', 'andorinha', 'urubu', 'porco']
-        ]
+
 
         while True:
             pts = 0
